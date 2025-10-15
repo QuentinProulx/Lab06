ba6f739e4a94d6bf1b7b1e99e2cdc2416d8e60dc
Quentin Proulx <6324569@edu.vaniercollege.qc.ca>
2025-10-15 11:29:18 -0400

    Finished task 2

--- src/lab06/Lab0602.java
+++ src/lab06/Lab0602.java
@@ -27,6 +27,10 @@ public class Lab0602 extends Application {
     @Override
     public void start(Stage stage) {
 
+        // Ground
+        Rectangle grass = new Rectangle(0, 400, 500, 100);
+        grass.setFill(Color.GREEN);
+        
         // House
         Rectangle baseHouse = new Rectangle(150, 200, 200, 200);
         baseHouse.setFill(Color.LIGHTGREY);
@@ -36,10 +40,42 @@ public class Lab0602 extends Application {
         roof.setFill(Color.RED);
         
         Rectangle base = new Rectangle(150, 400, 200, 10);
-        base.setFill(Color.WHEAT);
+        base.setFill(Color.BEIGE);
+        
+        Rectangle door = new Rectangle(225, 300, 50, 100);
+        door.setFill(Color.DARKRED);
+        
+        Rectangle chimney = new Rectangle(175, 100, 25, 75);
+        chimney.setFill(Color.GRAY);
+        
+        // Windows
+        Rectangle window1 = new Rectangle(275, 230, 50, 50);
+        window1.setFill(Color.LIGHTBLUE);
+        
+        Rectangle window2 = new Rectangle(175, 230, 50, 50);
+        window2.setFill(Color.LIGHTBLUE);
+        
+        Line v1 = new Line(200, 280, 200, 230);
+        Line v2 = new Line(300, 280, 300, 230);
+        
+        Line h1 = new Line(175, 255, 225, 255);
+        Line h2 = new Line(275, 255, 325, 255);
+        
+        // Sun
+        Circle sun = new Circle(450, 50, 40);
+        sun.setFill(Color.YELLOW);
+        
+        Line beam1 = new Line(400, 50, 375, 50);
+        beam1.setStroke(Color.YELLOW);
+        
+        Line beam2 = new Line(410, 85, 380, 115);
+        beam2.setStroke(Color.YELLOW);
+        
+        Line beam3 = new Line(450, 100, 450, 125);
+        beam3.setStroke(Color.YELLOW);
         
         // Running
-        Pane root = new Pane(baseHouse, roof, base);
+        Pane root = new Pane(grass, baseHouse, roof, base, chimney, door, window1, window2, v1, v2, h1, h2, sun, beam1, beam2, beam3);
         Scene scene = new Scene(root, 500, 500);
         
         stage.setTitle("House with Window Panes");
